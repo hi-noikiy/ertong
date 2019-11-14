3d7de68f9895c73d18acbe258979ae736266ff31
SunjiuYang <15945104936@163.com>
2019-11-14 23:19:52 +0800

    注释

--- addons/zjhj_mall/core/modules/api/controllers/OfficialController.php
+++ addons/zjhj_mall/core/modules/api/controllers/OfficialController.php
@@ -17,15 +17,9 @@ use app\models\Official;
 use app\modules\api\models\OfficialForm;
 use app\modules\api\behaviors\LoginBehavior;
 use app\models\Cabinet;
+header("Access-Control-Allow-Origin: *");
 class OfficialController extends Controller
 {
-    function __construct(){
-
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods:POST,GET");
-        header("Access-Control-Allow-Headers:x-requested-with,content-type");
-        header("Content-type:text/json;charset=utf-8");
-    }
     public function behaviors()
     {
         return array_merge(parent::behaviors(), [
