6f850df3709162eff512d0c171ebf7efe991d63d
SunjiuYang <15945104936@163.com>
2019-12-11 21:43:20 +0800

    注释

--- addons/zjhj_mall/core/modules/api/models/LoginFormMobile.php
+++ addons/zjhj_mall/core/modules/api/models/LoginFormMobile.php
@@ -158,7 +158,7 @@ class LoginFormMobile extends ApiModel
         $User->avatar_url = \Yii::$app->request->hostInfo . \Yii::$app->request->baseUrl . '/statics/images/avatar.png';
         $User->store_id = $this->store_id;
         $User->platform = 0; // 支付宝
-        $user->invitation_code = mt_rand(100000, 999999);
+        $User->invitation_code = mt_rand(100000, 999999);
         $User->binding=$contact_way;
         if($User->save()){
             $list_user_info = User::find()->where(['id'=>$User->id])->asArray()->one();
